7f191075ec867888c86eb2918d6c8233a26d3d4d
Ronald Albert <ronaldalbert1609@gmail.com>
2017-02-16 10:53:27 -0200

    LearningJqueryFunctions

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-	$('div').mouseenter(function(){
-		$('div').fadeTo('fast', 1);
+
+	$('div.p').append("<p>Good Bye</p>");
+	$('div.p').prepend("<p>Hello</p>");
+	$('div.p').after("<div><p> Isso esta depois do outro div </p></div>");
+	$('div.p').before("<div><p> Isso esta antes do outro div </p></div>")
+
+
+	$('div.click-me').mouseenter(function(){
+		var $target = $(this);
+		$target.fadeTo('fast', 1);
 	});
-	$('div').mouseleave(function(){
-		$('div').fadeTo('fast', 0.5);
+
+	$('div.click-me').mouseleave(function(){
+		var $target = $(this);
+		$target.fadeTo('fast', 0.5);
 	});
+
+	$('div.click-me').click(function(){
+		var $target = $(this);
+		$target.hide();
+	});
+
+	$('.pull-me').click(function(){
+		 $(".panel").slideToggle("slow");
+	});
+
 });
